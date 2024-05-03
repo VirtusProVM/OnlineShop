c1916e7976675edf03595071a55e468a8d21574d
VirtusProVM <92260039+VirtusProVM@users.noreply.github.com>
2024-05-03 18:51:03 +0200

    Update ListCategoryServlet.java

--- com/computerstore/category/servlet/ListCategoryServlet.java
+++ com/computerstore/category/servlet/ListCategoryServlet.java
@@ -13,10 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author vrle9
- */
 @WebServlet(name = "ListCategoryServlet", urlPatterns = {"/admin/categories"})
 public class ListCategoryServlet extends HttpServlet {
 
